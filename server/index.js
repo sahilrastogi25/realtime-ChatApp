6f7ffebc9320c9d4553c135f4e320ba05cd6f583
Sahil <sahil.rstg2598@gmail.com>
2021-07-02 10:23:47 +0530

    worked with redis to store user's offline and online info

--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
 require("dotenv").config();
 const express = require("express");
-const app = express();
+const http = require("http");
 const cors = require("cors");
-const Routes = require("./app/routes");
-app.use(express.json(), cors(), express.static("uploads"), Routes);
 const PORT = process.env.PORT;
-app.listen(PORT, () => {
+const app = express();
+const server = http.createServer(app);
+const Routes = require("./app/routes");
+
+app.use([express.json(), cors(), express.static("uploads"), Routes]);
+
+const io = (module.exports.io = require("socket.io")(server));
+const socketManager = require("./app/socketManager/socketManager.js");
+
+io.on("connection", socketManager);
+server.listen(PORT, () => {
   console.log(`Server started at PORT-${PORT}`);
 });
